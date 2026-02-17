b23f0d48688e0f91d3b1c2ef50ae936b5049aa01
SrajitRastogi <rastogisrajit@gmail.com>
2026-02-17 14:39:25 -0600

    Changed contact info

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
         <h2 className="text-4xl font-bold mb-6">Contact</h2>
   
         <p className="text-gray-400">
-          srajitrastogi03@gmail.com
+          rastogisrajit03@gmail.com
         </p>
       </section>
     );
